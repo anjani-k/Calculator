4a371c5fa2ddc6d3de9d84ba8524d3e905aea86f
anjani-k <anjanikolluru3@gmail.com>
2018-08-04 19:39:02 -0400

    Calculator

---
+++ Calc/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+    class Program
+    {
+       private static void Main(string[] args)
+        {
+        Calculator.Add(10, 20);
+        }
+    }
+public class Calculator
+{
+    [Obsolete]
+    public static int Add(int FirstNum, int SecondNum)
+    {
+        return FirstNum + SecondNum;
+    }
+    public static int Add(List<int> Numbers)
+    {
+        int sum = 0;
+        foreach(int Number in Numbers)
+        {
+            sum = sum + Number;
+        }
+    }
+}
+
